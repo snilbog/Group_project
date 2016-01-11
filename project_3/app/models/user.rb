8e1dee0a75588bb0aad9c3fb24151324b1757e57
Wyatt Williams <williams.wyatt@gmail.com>
2016-01-11 13:20:24 -0800

    fixed user model/migration

--- project_3/app/models/user.rb
+++ project_3/app/models/user.rb
@@ -3,14 +3,14 @@ class User < ActiveRecord::Base
   presence: true,
   uniqueness: {case_sensitive: false}
 
-  validates_presence_of :password, on: create
+  validates_presence_of :password, on: :create
 
   has_secure_password
 
-  has_many :favorite
   has_many :comment
+  has_many :favorite
 
   def self.authenticate email, password
-    User.find_by_email(email).try(:authenticate, password)
+      User.find_by_email(email).try(:authenticate, password)
   end
 end
