f198ee14420f76e24db40335eea388c9bc79abee
Wyatt Williams <williams.wyatt@gmail.com>
2016-01-14 15:41:02 -0800

    removed is_authenticated? from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 
-	before_action :is_authenticated?
 	#Don't have to be logged in to view drinks
 	# skip_before_action :is_authenticated?, only: [:index]
 
